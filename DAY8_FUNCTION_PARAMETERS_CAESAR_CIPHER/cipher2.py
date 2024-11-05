772b530ad51512f19e998573b515977e6ef3f897
zen2see <zen2see@gmail.com>
2024-11-04 19:05:45 -0500

    ADDED DAY8

--- DAY8_FUNCTION_PARAMETERS_CAESAR_CIPHER/cipher2.py
+++ DAY8_FUNCTION_PARAMETERS_CAESAR_CIPHER/cipher2.py
@@ -6,26 +6,36 @@ shift = int(input("Type the shift number:\n"))
 # cipher_text = ""
 # decrypted_text = ""
 
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
+# def encrypt(original_text, shift_amount):
+#     cipher_text = ""
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) + shift_amount
+#         shifted_position %= len(alphabet)
+#         cipher_text += alphabet[shifted_position]
+#     print(f"Here is the encoded result: {cipher_text}")
+
+# def decrypt(coded_text, shift_amount):
+#     decrypted_text = ""
+#     for letter in coded_text:
+#         unshifted_position = alphabet.index(letter) - shift_amount 
+#         unshifted_position %= len(alphabet)
+#         print(unshifted_position)
+#         decrypted_text += alphabet[unshifted_position]
+#     print(f"The decrypted text is: {decrypted_text}")
+
+# if direction == 'encode':
+#     encrypt(original_text=text, shift_amount=shift)
+# else:
+#     decrypt(coded_text=text, shift_amount=shift)
+
+def caesar(original_text, shift_amount, direction):
+    output_text = ""
+    if direction == "decode":
+        shift_amount *= -1
     for letter in original_text:
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+        output_text += alphabet[shifted_position]
+    print(f"Here is the {direction}d result: {output_text}")
 
-def decrypt(coded_text, shift_amount):
-    decrypted_text = ""
-    for letter in coded_text:
-        unshifted_position = alphabet.index(letter) + shift_amount 
-        unshifted_position %= len(alphabet)
-        print(unshifted_position)
-        decrypted_text += alphabet[unshifted_position]
-    print(f"The decrypted text is: {decrypted_text}")
-
-if direction == 'encode':
-    encrypt(original_text=text, shift_amount=shift)
-else:
-    decrypt(coded_text=text, shift_amount=shift)
-    
-#encrypt(original_text=text, shift_amount=shift)
+caesar(original_text=text, shift_amount=shift, direction=direction)
---
+++ DAY9_DICTIONARIES_NESTING_SECRET_AUCTION/listinsidelist.py
@@ -0,0 +1,7 @@
+nested_list = ["A", "B", ["C", "D"]]
+# Try to print "D" from the list nested_list.
+print(nested_list[0][0]) # = A
+print(nested_list[1][0]) # = B
+print(nested_list[2][0]) # = C
+print(nested_list[2][1]) # = D
+print(nested_list)
