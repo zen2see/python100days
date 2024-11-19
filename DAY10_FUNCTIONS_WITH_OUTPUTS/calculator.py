bb7b4af91872a8d56722ea724e1133bae1dcd807
zen2see <zen2see@gmail.com>
2024-11-19 16:48:21 -0500

    COMPLETED DAY10

--- DAY10_FUNCTIONS_WITH_OUTPUTS/calculator.py
+++ DAY10_FUNCTIONS_WITH_OUTPUTS/calculator.py
@@ -1,8 +1,10 @@
+import art
+
 def add(n1, n2):
     return n1 + n2
 
 def subtract(n1, n2):
-    return n2 - n1
+    return n1 - n2
 
 def multiply(n1, n2):
     return n1 * n2
@@ -16,9 +18,24 @@ operations = {
     "*": multiply,
     "/": divide
 }
-num1 = float(input("What is the first number? "))
-for symbol in operations:
-    print(symbol)
-num2 = float(input("What is the second number? "))
-op = input("Pick an operation: ")
-print(operations[op](4, 8))
+
+def calculator():
+    print(art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number? "))
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        op = input("Pick the operation to perform: ")
+        num2 = float(input("What is the second number? "))
+        ans = operations[op](num1, num2)
+        print(f"{num1} {op} {num2} = {ans}")
+        choice = input(f"Type 'y' to continue calculating with {ans}, or type 'n' to start over. ")    
+        if choice == 'y':
+           num1 = ans
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
