f6234b343e2967117f4fb3eaddea6d0d0cd67090
zen2see <zen2see@gmail.com>
2024-11-20 18:50:44 -0500

    UPDATE DAY11 blackjack

---
+++ DAY11_BLACKJACK_CAPSTONEPROJECT/blackjack.py
@@ -0,0 +1,82 @@
+from os import system, name
+import random
+import artblackjack
+   
+def calculate_score(cards):
+    """
+    Take a list of cards and return the score calculated
+    from the cards
+    """
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum (cards)
+
+
+x = 1
+players = []
+dealer = []
+playercards = []
+dealercards = []
+cards = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'J':10, 'Q':10, 'K':10, 'A':11} 
+gameover = False
+
+# define our clear function
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+def randomcard():
+    """
+    Returns a random card from the deck
+    """
+    # Get random dictionary pair in dictionary
+    # Using random.choice() + list() + items()
+    random_card = random.choice(list(cards.items()))
+    # OR random.choice(cards)
+    """
+    print("The random card is " + str(random_card))
+    print(random_card[0])
+    print(random_card[1])
+    """
+    # print("The random card is " + str(random_card[0]))
+    return random_card[0]
+
+def playblackjack():
+    print(artblackjack.logo)
+    play = input("Another to play a game of Blackjack? Type 'y' or 'n' ").lower()
+    if play == 'y':
+        players.append(1)
+        clear()
+        playblackjack()
+    elif play == 'n' and players == 0:
+       quit()
+    else:
+        blackjack()
+
+def blackjack():
+    print(artblackjack.logo)
+    # print(randomcard())
+    # dealer.update(randomcard())
+    for _ in range(2):
+         # for n in players:
+         # players.append(playercards.append[randomcard()])
+         # print(f"Player{n}'s card {players[n]}")
+         playercards.append(randomcard())
+         playerscore = calculate_score(playercards)
+         print(f"Player's cards {playercards} score = {playerscore}")
+         dealercards.append(randomcard())
+         dealerscore = calculate_score(dealercards)
+         print(f"Dealer's cards {dealercards} score = {dealerscore}")
+
+    if playerscore == 0 or dealerscore == 0 or playerscore > 21:
+        game_over = True
+playblackjack()
+
+    
