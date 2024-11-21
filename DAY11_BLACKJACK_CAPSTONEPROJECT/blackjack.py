59d0ee123c4a28dd782cc18a9ee2bd08d6a91e28
zen2see <zen2see@gmail.com>
2024-11-21 18:58:26 -0500

    UPDATED blackjack.py

--- DAY11_BLACKJACK_CAPSTONEPROJECT/blackjack.py
+++ DAY11_BLACKJACK_CAPSTONEPROJECT/blackjack.py
@@ -7,6 +7,9 @@ players = []
 dealer = []
 playercards = []
 dealercards = []
+playerscore = -1 
+dealerscore = -1
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] 
 cardsl = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'J':10, 'Q':10, 'K':10, 'A':11}
 gameover = False
 
@@ -27,7 +30,6 @@ def randomcard():
     # random_card = random.choice(list(cards.items()))
     # OR random.choice(cards)
     """ 
-    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10] 
     card = random.choice(cards)
     # print("The random card is " + str(random_card))
     # print(random_card[0])
@@ -43,7 +45,7 @@ def calculate_score(cards):
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
-    return sum (cards)
+    return sum(cards)
 
 def comparescores(playerscore, dealerscore):
     if playerscore == dealerscore:
@@ -52,7 +54,16 @@ def comparescores(playerscore, dealerscore):
         return "Lose, opponent has Blackjack"
     elif playerscore == 0:
         return "Win you have Blackjack"
+    elif playerscore > 21:
+        return "Lose, you went over 21"
+    elif dealerscore > 21:
+        return "Opponent went over. You win"
+    elif playerscore > dealerscore:
+        return "You win!"
+    else: 
+        return "You lose!"
 
+"""
 def playblackjack():
     play = input("New player? Join a game of Blackjack? Type 'y' or 'n' ").lower()
     if play == 'y':
@@ -65,30 +76,69 @@ def playblackjack():
         blackjack()
 
 def blackjack():
-    print(artblackjack.logo)
-    # print(randomcard())
-    # dealer.update(randomcard())
-    for _ in range(2):
-         # for n in players:
-         # players.append(playercards.append[randomcard()])
-         # print(f"Player{n}'s card {players[n]}")
-         playercards.append(randomcard())
-         dealercards.append(randomcard())
+    global gameover
+    while not gameover:
+        print(artblackjack.logo)
+        blackjack()
+        # print(randomcard())
+        # dealer.update(randomcard())
+        for _ in range(2):
+            # for n in players:
+            # players.append(playercards.append[randomcard()])
+            # print(f"Player{n}'s card {players[n]}")
+            playercards.append(randomcard())
+            dealercards.append(randomcard())
         
+        playerscore = calculate_score(playercards)
+        print(f"Player's cards {playercards} score = {playerscore}")
+        dealerscore = calculate_score(dealercards)
+        print(f"Dealer's cards {dealercards} score = {dealerscore}")
+        if playerscore == 0 or dealerscore == 0 or playerscore > 21:
+            gameover = True
+        else:
+            dealagain = input("Type 'y' to get another card, type 'n' to pass: ")
+            if dealagain == 'y':
+                playercards.append(randomcard())
+            else:
+                gameover = True
+
+playblackjack()
+"""
+
+for _ in range(2):
+    playercards.append(randomcard())
+    dealercards.append(randomcard())
+
+while not gameover:
+    # print(artblackjack.logo)
     playerscore = calculate_score(playercards)
     print(f"Player's cards {playercards} score = {playerscore}")
     dealerscore = calculate_score(dealercards)
     print(f"Dealer's cards {dealercards} score = {dealerscore}")
+
     if playerscore == 0 or dealerscore == 0 or playerscore > 21:
-        game_over = True
+        gameover = True
     else:
         dealagain = input("Type 'y' to get another card, type 'n' to pass: ")
         if dealagain == 'y':
             playercards.append(randomcard())
         else:
             gameover = True
-
-while not gameover:
-    playblackjack()
-
     
+while dealerscore != 0 and dealerscore < 17:
+    dealercards.append(randomcard)
+    dealerscore = calculate_score(dealercards)
+    # print(f"Dealer's cards {dealercards} score = {dealerscore}")
+
+print(f"Your final hand: {playercards}, final score: {playerscore}")
+print(f"Dealers final hand: {dealercards}, final score: {dealerscore}")
+print(comparescores(playerscore, dealerscore))
+ #       for _ in range(2):
+            # for n in players:
+            # players.append(playercards.append[randomcard()])
+            # print(f"Player{n}'s card {players[n]}")
+    #        playercards.append(randomcard())
+   #         dealercards.append(randomcard())
+        
+        
+        
