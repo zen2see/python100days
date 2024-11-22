5e42bf596c6d4cb3ea512d0802d206c502a8dd50
zen2see <zen2see@gmail.com>
2024-11-22 18:28:28 -0500

    UPDATE DAY11 blackjack calculate_score()

--- DAY11_BLACKJACK_CAPSTONEPROJECT/blackjack.py
+++ DAY11_BLACKJACK_CAPSTONEPROJECT/blackjack.py
@@ -9,7 +9,7 @@ playercards = []
 dealercards = []
 playerscore = -1 
 dealerscore = -1
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] 
+
 cardsl = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'J':10, 'Q':10, 'K':10, 'A':11}
 gameover = False
 
@@ -30,6 +30,7 @@ def randomcard():
     # random_card = random.choice(list(cards.items()))
     # OR random.choice(cards)
     """ 
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] 
     card = random.choice(cards)
     # print("The random card is " + str(random_card))
     # print(random_card[0])
@@ -42,9 +43,11 @@ def calculate_score(cards):
     """
     if sum(cards) == 21 and len(cards) == 2:
         return 0
+    
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
+
     return sum(cards)
 
 def comparescores(playerscore, dealerscore):
@@ -114,8 +117,8 @@ while not gameover:
     playerscore = calculate_score(playercards)
     print(f"Player's cards {playercards} score = {playerscore}")
     dealerscore = calculate_score(dealercards)
-    print(f"Dealer's cards {dealercards} score = {dealerscore}")
-
+    # print(f"Dealer's cards {dealercards} score = {dealerscore}")
+    print(f"Dealer 1st card {dealercards[0]}")
     if playerscore == 0 or dealerscore == 0 or playerscore > 21:
         gameover = True
     else:
---
+++ DAY11_BLACKJACK_CAPSTONEPROJECT/test.py
@@ -0,0 +1,14 @@
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] 
+
+def calculate_score(cards):
+    """
+    Take a list of cards and return the score calculated from the cards
+    """
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)        
+calculate_score
