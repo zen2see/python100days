05fc469abc8374515d86bb046552764f33fdcb76
zen2see <zen2see@gmail.com>
2024-12-18 20:14:15 -0500

    UPDATE DAY12 banking.py

--- DAY12_SCOPE_AND_NUMBER_GUESSING_GAME/banking.py
+++ DAY12_SCOPE_AND_NUMBER_GUESSING_GAME/banking.py
@@ -5,13 +5,8 @@ import artbanking
 attempts = 0
 guess = 0
 number = random.randint(1, 100)
-
-def main():    
-    print(artbanking.logo)
-    print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 100")
-    set_difficulty()
-    guess_number()
+EASY_LEVEL = 10
+HARD_LEVEL = 5
 
 def playagain():
     again = input("Play again - 'y' or 'n': ").lower()
@@ -22,11 +17,28 @@ def playagain():
         
 def set_difficulty():
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-    global attempts
+    
     if difficulty == "easy":
-        attempts = 10
+        return EASY_LEVEL
     else: 
-        attempts = 4
+        return HARD_LEVEL
+
+def check_answer(guess, number, attempts):
+    attempts
+    while attempts > -1:
+        if guess > number:
+            attempts -= 1
+            print("Too high.\nGuess again.\n")
+            guess_number(attempts)
+        elif guess < number:
+            attempts -= 1
+            print("Too low.\nGuess again.\n")
+            guess_number()
+        else:
+            won()
+        print("You ran out of guesses!")
+        lost() 
+
 
 def lost():
     print("You lost!")
@@ -37,20 +49,31 @@ def won():
     playagain() 
 
 def guess_number():
-    global attempts
     print(f"\nYou have {attempts} remaining chances to guess the number")
     guess = int(input(f"Make a guess: "))
-    while attempts > -1:
-        if guess > number:
-            print("Too high.\nGuess again.\n")
-            guess_number()
-        elif guess < number:
-            print("Too low.\nGuess again.\n")
-            guess_number()
-        else:
-            won()
-        print("You ran out of guesses!")
-        lost() 
+    check_answer(guess, number, attempts)
+    # check_answer(guess, number, attempts)
+    # while attempts > 0:
+    #     if guess > number:
+    #         attempts -= 1
+    #         print("Too high.\nGuess again.\n")
+    #         guess_number()
+    #     elif guess < number:
+    #         attempts -= 1
+    #         print("Too low.\nGuess again.\n")
+    #         guess_number()
+    #     else:
+    #         won()
+    #     print("You ran out of guesses!")
+    #     lost() 
+
+def main(): 
+    print(artbanking.logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100")
+    global attempts
+    attempts = set_difficulty()
+    guess_number()
 
 if __name__ == "__main__":
     main()
