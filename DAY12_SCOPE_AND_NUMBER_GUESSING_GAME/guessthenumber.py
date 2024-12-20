1b1d1cb4e7229a384c82bc9085ac09f7c303ca88
zen2see <zen2see@gmail.com>
2024-12-20 12:30:45 -0500

    UPDATED banking.py to guessthenumber.py

--- DAY12_SCOPE_AND_NUMBER_GUESSING_GAME/guessthenumber.py
+++ DAY12_SCOPE_AND_NUMBER_GUESSING_GAME/guessthenumber.py
@@ -2,13 +2,12 @@ import datetime
 import random
 import artbanking
 import sys
-import curses
 import time
 
 # Banking Guessing Game!
 attempts = 0
 guess = 0
-number = random.randint(1, 100)
+
 EASY_LEVEL = 10
 HARD_LEVEL = 5
 firstrun = True
@@ -20,15 +19,14 @@ def clock():
         print(datetime.datetime.now().strftime("%H:%M:%S"), end="\r")
         time.sleep(1)
 
+def getRandomNum():
+    num = random.randint(1, 100)
+    return num
+
 def clear_part(lines):
     for _ in range(lines):
         sys.stdout.write(CURSOR_UP_ONE) 
         sys.stdout.write(ERASE_LINE) 
-
-# def clear_part(stdscr):
-#     for i in range(y, y + height):
-#         stdscr.addstr(i, x, " " * width)
-#     stdscr.refresh()
 
 def playagain():
     again = input("Play again - 'y' or 'n': ").lower()
@@ -37,8 +35,7 @@ def playagain():
     else:
         exit()
         
-def set_difficulty():
-    global firstrun
+def set_difficulty(firstrun):
     firstrun = False
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
     if difficulty == "easy":
@@ -48,44 +45,45 @@ def set_difficulty():
 
 def check_answer(guess, number, attempts):
     clear_part(4)
-    while attempts > -1:
+    while attempts > 0:
         if guess > number:
             attempts -= 1
-            print("Too high.\nGuess again.\n")
-            guess_number(attempts)
+            print(guess, "Too high. Guess again:\n")
+            guess_number(attempts, number)
         elif guess < number:
             attempts -= 1
-            print("Too low.\nGuess again.\n")
-            guess_number(attempts)
+            print(guess, "Too low. Guess again:\n")
+            guess_number(attempts, number)
         else:
             won()
         print("You ran out of guesses!")
-        lost() 
+        lost(number) 
 
-
-def lost():
+def lost(num):
     print("You lost!")
+    print("It was ", num)
     exit()
 
 def won():
     print("YOU GUESSED THE NUMBER!")
     playagain() 
 
-def guess_number(attempts):
-    if firstrun == False:
-        clear_part(2)
-    print(f"\nYou have {attempts} remaining chances to guess the number")
+def guess_number(attempts, number):
+    # if firstrun == False:
+    #     clear_part(2)
+    print(f"You have {attempts} remaining chances to guess the number")
     guess = int(input(f"Make a guess: "))
     check_answer(guess, number, attempts)
 
 def main(): 
     # clock()
+    number = getRandomNum()
     print(artbanking.logo)
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100")
-    global attempts
-    attempts = set_difficulty()
-    guess_number(attempts)
+    # global attempts
+    attempts = set_difficulty(firstrun)
+    guess_number(attempts, number)
 
 if __name__ == "__main__":
     main()
