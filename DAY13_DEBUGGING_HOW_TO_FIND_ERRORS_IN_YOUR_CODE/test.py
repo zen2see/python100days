64bca85de32618d6cc41fbc1c731d5eadcc0703d
zen2see <zen2see@gmail.com>
2024-12-21 19:13:42 -0500

    ADDED DAY13

---
+++ DAY13_DEBUGGING_HOW_TO_FIND_ERRORS_IN_YOUR_CODE/test.py
@@ -0,0 +1,7 @@
+def my_function():
+    # for i in range(1,20):
+    for i in range(1, 21):
+        print(i)
+        if i == 20:
+            print("You got it")
+my_function()
---
+++ DAY13_DEBUGGING_HOW_TO_FIND_ERRORS_IN_YOUR_CODE/test0.py
@@ -0,0 +1,5 @@
+from random import randint
+dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+dice_num = 6
+print(dice_images[dice_num])
