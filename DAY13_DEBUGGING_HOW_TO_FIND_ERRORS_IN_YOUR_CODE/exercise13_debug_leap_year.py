d36aa27c9aa858e12ca4415e8e15d03cb353317c
zen2see <zen2see@gmail.com>
2024-12-26 19:53:11 -0500

    UPDATE exercise13

--- DAY13_DEBUGGING_HOW_TO_FIND_ERRORS_IN_YOUR_CODE/exercise13_debug_leap_year.py
+++ DAY13_DEBUGGING_HOW_TO_FIND_ERRORS_IN_YOUR_CODE/exercise13_debug_leap_year.py
@@ -1,11 +1,14 @@
 def is_leap(year):
     if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 4000 == 0:
+        if year % 100 != 0:
+            if year % 400 != 0:
                 return True
             else:
                 return False
-        else:
+        elif year % 100 == 0 and year % 400 == 0:
             return True
+        else:
+            return False
     else:
         return False
+print(is_leap(2020))
