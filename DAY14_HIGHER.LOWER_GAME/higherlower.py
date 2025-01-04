73c1a73c2545e546b6ec3d1718280f090bc1e950
zen2see <zen2see@gmail.com>
2025-01-04 17:15:58 -0500

    UPDATED DAY14 higherlower

--- DAY14_HIGHER.LOWER_GAME/higherlower.py
+++ DAY14_HIGHER.LOWER_GAME/higherlower.py
@@ -1,10 +1,19 @@
-from higherlowerlogo import logo
+from gamedata import data 
+from higherlowerlogo import higherlowerlogo as hllogo
+import random
 
 # Display Logo
-print(logo)
+def printlogo():
+    print(hllogo)
+
+# Get Topics
+random_topic1 = random.choice(data)
+random_topic2 = random.choice(data)
 
 # Ask Question
-
+def askq():
+    printlogo()
+   
 # Display Results
 
 # Display Score
--- DAY14_HIGHER.LOWER_GAME/testgame.py
+++ DAY14_HIGHER.LOWER_GAME/testgame.py
@@ -1,5 +1,8 @@
 from gamedata import data 
+import random
 
+random_topic1 = random.choice(data)
+print(random_topic1)
 """
 print(data[2]['follower_count'])
 print(data[3]['follower_count'])
@@ -8,6 +11,3 @@ if data[2]['follower_count'] > data[3]['follower_count']:
 else:
     print(False)
 """
-# Ask Question
-def askq:
-   
