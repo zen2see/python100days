ecc34affc42bf5eded14ec61abbc6d78222076fe
zen2see <zen2see@gmail.com>
2025-01-09 11:51:01 -0500

    UPDATED DAY14_HIGHER.LOWER_GAME no globals

--- DAY14_HIGHER.LOWER_GAME/higherlower.py
+++ DAY14_HIGHER.LOWER_GAME/higherlower.py
@@ -6,12 +6,6 @@ import random
 """ From the list of unicodes, replace “+” with “000”. https://home.unicode.org/ www.unicode.org/
  example – “U+1F600” will become “U0001F600” and prefix the unicode with “\” and print it. """
 
-ans = "" 
-score = 0
-should_continue = True
-random_topic1 = random.choice(data)
-random_topic2 = random.choice(data)
-
 # Display Logo
 def printlogo():
     print(hllogo)
@@ -23,26 +17,25 @@ def randomtopics():
     return random_topic1, random_topic2
 
 # Compare Topics 
-def compareT():
+def compareT(random_topic1, random_topic2):
    print(f"A: {random_topic1['name']}, a {random_topic1['description']}, from {random_topic1['country']}.")
    print(vslogo)
    print(f"B: {random_topic2['name']}, a {random_topic2['description']}, from {random_topic2['country']}.") 
 
 # Ask Question
 def askq():
-    global ans 
     ans = input("\nWho has more followers? Type 'A' or 'B': ").lower()
+    return ans
 
 # Display Results
-def results():
+def results(random_topic1, random_topic2):
     return max(random_topic1['follower_count'],random_topic2['follower_count'])
 
-def check():
-    global should_continue, score
-    if (results() == random_topic1['follower_count']) & (ans == 'a'):
+def check(random_topic1, random_topic2, ans, score):
+    if (results(random_topic1, random_topic2) == random_topic1['follower_count']) & (ans == 'a'):
         print("Correct! \U0001F600")
         score += 1
-    elif (results() == random_topic2['follower_count']) & (ans == 'b'):
+    elif (results(random_topic1, random_topic2) == random_topic2['follower_count']) & (ans == 'b'):
         print("Correct! \U0001F600")
         score += 1
     else:
@@ -50,22 +43,26 @@ def check():
     print(f"{random_topic1['follower_count']} vs. {random_topic2['follower_count']}")
     print("Score is ", score)
     cont = input("Should we continue? 'Y' or 'N': ").lower()
+    should_continue = cont == 'y'
     if cont == 'y':
         should_continue = True
     else: 
         should_continue = False
+    return should_continue, score
 
 def main():
     printlogo()
+    score = 0
+    should_continue = True
+    random_topic1, random_topic2 = randomtopics()
     while should_continue == True:
-        global random_topic1, random_topic2
         random_topic1 = random_topic2 
         random_topic2 = random.choice(data)
         if random_topic1 == random_topic2:
           random_topic2 = random.choice(data)
-        compareT()
-        askq()
-        check()
+        compareT(random_topic1, random_topic2)
+        ans = askq()
+        should_continue, score = check(random_topic1, random_topic2, ans, score)
 
 if __name__ == '__main__':
     main()
