c443e11e4307cea504bf73133496446222520cd3
zen2see <zen2see@gmail.com>
2025-01-15 20:20:55 -0500

    UPDATE DAY15_COFFEE_MACHINE_PROJECT

--- DAY15_COFFEE_MACHINE_PROJECT/coffeemachine.py
+++ DAY15_COFFEE_MACHINE_PROJECT/coffeemachine.py
@@ -1,14 +1,10 @@
 import sys
 import time
 from os import system, name
-
+#
+print(sys.executable)
 """ From the list of unicodes, replace “+” with “000”. https://home.unicode.org/ www.unicode.org/
  example – “U+1F600” will become “U0001F600” and prefix the unicode with “\” and print it. """
-
-Water = 500
-Milk = 250
-Coffee = 380
-Money = 12.50
 
  # COFFEE 16g/.55oz for Water 236ml/8oz 
  # WATER 325 ml = 8oz / 1oz = 30ml/28g
@@ -27,18 +23,32 @@ def clear():
     else:
         _ = system('clear')
 
+def update(w, m, c, mo, Water, Milk, Coffee, Money):
+    Water -= w
+    Milk -= m
+    Coffee -= c
+    Money += mo
+    return Water, Milk, Coffee, Money
+
 # Prompt 
 def prompt(ask):      
-    ans = input(f"{ask} (espresso/latte/cappuccino/report) ").lower()
+    ans = input(f"{ask} (espresso/latte/cappuccino/report)? ").lower()
     return ans
 
-# Answer/reply
-def answer(ans):
+def noingredients(ingredient):
+    if ingredient == "Water":
+        print("Sorry there is not enough Water")
+    elif ingredient == "Coffee":
+        print("Sorry there is not enough Coffee")
+    elif ingredient == "Milk":
+        print("Sorry there is not enough Milk")
+
+def answer(ans, Water, Milk, Coffee, Money):
     if (ans == "off"):
         # Exit with status code 0 (success)
         sys.exit(0) 
     elif (ans == "report"):
-        print(f"\nWater: {Water}\nMilk: {Milk}\nCoffee: {Coffee}\nMoney: {Money}")
+        report(Water, Milk, Coffee, Money)
     elif ans == "espresso":
         """
         The ratio of coffee to water for espresso can vary depending on the type of espresso: 
@@ -52,33 +62,68 @@ def answer(ans):
         """
         # ESPRESSO Heaped teaspoon and Hot Water (one fluid ounce and a double is two)
         # 70 ml coffee.  
-        if Water > 60 & Coffee > 30:
+        if Water >= 60:
             Water -= 60
+        else:
+            noingredients(Water)
+        if Coffee >= 30:    
             Coffee -= 30
-            return "espresso"
+            print("Espresso...")
+        else:
+            noingredients(Coffee)
     elif (ans == "latte"):
         # LATTE one shot of espresso 8-10oz of steamed milk 1/2 inch of milk foam
         # 70ml coffee, 8oz of milk,  1/2 inch of milk foam   
-        if  Water > 60 & Milk > 200 & Coffee > 20:
-           pass
+        if Water >= 60:
+            Water -= 60
+        else:
+            noingredients(Water)
+        if Coffee >= 30:
+            Coffee -= 30
+        else:
+            noingredients(Coffee)
+        if Milk >= 200:
+            Milk -= 200
+            return print("Latte...")
+        else: 
+           noingredients(Milk)
     elif (ans == "cappuccino"):
-        pass
-    
-def report():
+        if Water >= 60:
+            Water -= 60
+        else:
+            noingredients(Water)
+        if Coffee >= 30:
+            Coffee -= 30
+        else:
+            noingredients(Coffee)
+        if Milk >= 100:
+            Milk -= 100
+            return print("Cappuccino...")
+        else:
+            noingredients(Milk)
+            
+def report(Water, Milk, Coffee, Money):
     print(f"\nWater: {Water}\
             \nMilk: {Milk}\
             \nCoffee: {Coffee}\
-            \n)Money: $ {Money}\
-          ")
+            \nMoney: $ {Money}\
+        ")
 
-def main():   
+def main():
+    Water = 500
+    Milk = 250
+    Coffee = 380
+    Money = 12.50  
     ans = "on"
-    ask = "What would you like "
+    ask = "What would you like"
     while ans != "off":
         ans = prompt(ask)
-        answer(ans)
+        #Water, Milk, Coffee, Money = answer(ans, Water, Milk, Coffee, Money) 
+        answer(ans, Water, Milk, Coffee, Money)
         time.sleep(3)
-        print("\nDone!")
+        #answer(ans)
+        time.sleep(3)
+        print("\nDone!\n")
         time.sleep(2)
         clear()
         ask = "\nWould you like another "
