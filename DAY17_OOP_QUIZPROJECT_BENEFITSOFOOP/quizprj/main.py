88f658c788724864325482cac3a5f4e7e5614b3d
zen2see <zen2see@gmail.com>
2025-02-03 22:46:09 -0500

    UPDATED DAY17_OOP

--- DAY17_OOP_QUIZPROJECT_BENEFITSOFOOP/quizprj/main.py
+++ DAY17_OOP_QUIZPROJECT_BENEFITSOFOOP/quizprj/main.py
@@ -1,22 +1,38 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
-import sys
+from os import system, name
+import time
 
-question_bank = [Question(q["text"], q["answer"]) for q in question_data]
-qlength = len(question_bank)
 
-# Create an instance of the QuizBrain class
-quiz = QuizBrain(question_bank)
+
+# Clear function
+def clear():
+    """ CLear the screen. """
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 def main():
-    qnum = quiz.question_number
-    # nextq = True
-    while qnum < 3:
-        user_response = quiz.askq()   
-        print(f"Your answer: {user_response}") 
-        qnum = quiz.question_number
-    sys.exit()
-        
+    # Create Question format
+    question_bank = [Question(q["text"], q["answer"]) for q in question_data]
+    # Create an instance of the QuizBrain class
+    quiz = QuizBrain(question_bank)
+    while quiz.more_questions != True:
+        user_response = quiz.askq() 
+        quiz.check_answer(user_response, quiz.the_answer())  
+        # time.sleep(2)
+        print("Press Enter to continue...")
+        quiz.question_number +=1
+        input()
+        clear()
+    clear()
+    print("Good game. Thank you! Goodbye...")
+    time.sleep(2)
+    system.exit()
+
 if __name__ == '__main__':
     main()
--- DAY17_OOP_QUIZPROJECT_BENEFITSOFOOP/quizprj/quiz_brain.py
+++ DAY17_OOP_QUIZPROJECT_BENEFITSOFOOP/quizprj/quiz_brain.py
@@ -1,13 +1,40 @@
+from os import system
+
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.question_list_max = len(self.question_list)
+        self.score = 0
 
+    def more_questions(self): 
+        return self.question_number < len(self.question_list)
+      
     def askq(self):
-        current_question = self.question_list[self.question_number]
-        user_answer = input(f"Q{self.question_number + 1}: {current_question.text} (True/False): ")
-        self.question_number += 1
-        return user_answer
-
+        if self.more_questions():
+            current_question = self.question_list[self.question_number]
+            user_answer = input(f"Q{self.question_number + 1}: {current_question.text} (True/False): ").lower()
+            return user_answer
+        else:
+            print("No more questions available.")
+            return False
+    
+    def the_answer(self):
+        if self.more_questions():
+            theanswer = self.question_list[self.question_number].answer.lower()
+            return theanswer
+        else:
+            return None
+    
+    def check_answer(self, user_answer, answer):
+        print(user_answer, answer)
+        if user_answer == answer:
+            print("Correct!")
+            self.score += 1
+        else:
+            print("Incorrect")
+        print(f"The correct answer is: {self.the_answer()}")
+        print(f"Your current score is: {self.score}/{self.question_number + 1}")
+        print("\n")
     
     
