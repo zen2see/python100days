fd4f56d4f7011bed9eca8bab1b2750c8b869dfff
zen2see <zen2see@gmail.com>
2025-02-18 20:17:43 -0500

    UPDATED DAY20_SNAKE_GAME movements and food

--- DAY20_SNAKE_GAME/snake.py
+++ DAY20_SNAKE_GAME/snake.py
@@ -2,6 +2,7 @@ import time
 from turtle import Turtle, Screen
 from snakesegment import Snakesegment
 import random
+from food import Food
 
 """
 Pointer: ((15,0),(0,25),(-15,0),(0,3))
@@ -17,39 +18,28 @@ Pentagon: ((-5,10),(-10,0),(-5,-10),(5,-10),(10,0),(5,10))
 Hexagon: ((-10,20),(-20,0),(-10,-20),(10,-20),(20,0),(10,20))
 Octagon: ((-10,20),(10,20),(20,10),(20,-10),(10,-20),(-10,-20),(-20,-10),(-20,10))
 """
+# Using Turtle.distance
+def checkdist(snake, food):
+    if snake.head.distance(food) < 16:
+        food.goto(random.randint(-280, 280), random.randint(-280, 280))
+        snake.add_segment((Snakesegment.xcor, Snakesegment.ycor))
+        print("Collision")
 
-t = Turtle(shape="square")
-t.shapesize(1,1)
-
-snake = Snakesegment()
-
-# Draw a rectangle
-# def rectangle(width, height):
-#     for _ in range(2):
-#         t.forward(width)
-#         t.left(90)
-#         t.forward(height)
-#         t.left(90)
-
-# # Draw Segments
-# def create_segments(start_position):
-#     segments = [] 
-#     for position in start_position:
-#         segment = Turtle(shape="square")
-#         segment.color('blue')
-#         segment.penup()
-#         segment.goto(position)
-#         segments.append(segment)
-#     return segments
-
-# def move(segments):
-#    for seg_num in range(len(segments) -1, 0, -1):
-#            newx = segments[seg_num -1].xcor()
-#            newy = segments[seg_num -1].ycor()
-#            segments[seg_num].goto(newx, newy)
-#    segments[0].forward(20)
+def setup_screen():
+    screen = Screen()
+    screen.title('Snake Game')
+    screen.bgcolor('black')
+    screen.colormode(255)
+    screen.setup(width=600, height=600)
+    screen.tracer(0)
+    return screen
 
 def main():
+    t = Turtle(shape="square")
+    t.shapesize(1,1)
+    snake = Snakesegment()
+    screen = setup_screen()
+
     """
     register_shape() function. The first argument is what you will name the shape; 'rectangle',
     in this case. The second argument is the coordinates, so save them as a variable.
@@ -59,31 +49,26 @@ def main():
     #t = Turtle(shape='square')
     # squareCors = (20,20)
     # t.register_shape('square', squareCors)
-    screen = Screen()
-    screen.title('Snake Game')
-    screen.bgcolor('black')
-    screen.colormode(255)
-    screen.setup(width=600, height=600)
-
+    
     screen.listen()
     screen.onkey(snake.up, "Up")
     screen.onkey(snake.down, "Down")
     screen.onkey(snake.left, "Left")
     screen.onkey(snake.right, "Right")
 
-    # Set the dimensions of the rectangle
-    rectangle_width = 200
-    rectangle_height = 100
-
     # Hide the turtle
     t.hideturtle()
+
+    # Add food
+    food = Food()
 
     game_on = True
     while game_on:
         screen.update()
         time.sleep(0.1)
         snake.move()
-            
+        checkdist(snake, food)
+
     screen.exitonclick()   
     screen.mainloop() 
 
--- DAY20_SNAKE_GAME/snakesegment.py
+++ DAY20_SNAKE_GAME/snakesegment.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+import time
+import sys
 
 # Starting positions
 STARTING_POSITION = [(0,0), (-20,0), (-40,0)]
@@ -8,10 +10,12 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
-class Snakesegment:
+class Snakesegment(Turtle):
     def __init__(self):
+        #super().__init__()
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -20,11 +24,43 @@ class Snakesegment:
             asegment.penup()
             asegment.goto(position)
             self.segments.append(asegment)
-    
+
+    def add_segment(self, position):
+        # Get the position of the last segment
+        last_segment = self.segments[-1]
+        last_x = last_segment.xcor()
+        last_y = last_segment.ycor()
+        position = (last_x, last_y)
+        # new_segment = Snakesegment()
+        # new_segment.goto(last_x, last_y)
+        # self.segments.append(new_segment)
+        segment = Turtle(shape="square")
+        segment.color('blue')
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
+
+        # Create a new segment at the last segment's position
+        ##new_segment = Snakesegment()
+        ##new_segment.goto(last_x, last_y)
+
+        # Add the new segment to the snake
+        ##self.segments.append(new_segment)
+    def extend(self):
+        # Add a segment to the snake at the position of the last segment
+        last_segment = self.segments[-1]
+        position = last_segment.position()
+        self.add_segment(position)
+
     def move(self):
         for seg_num in range(len(self.segments) -1, 0, -1):
             newx = self.segments[seg_num -1].xcor()
             newy = self.segments[seg_num -1].ycor()
+            if newx >= 280 or newy >= 290 or newx <= -285 or newy <= -280:
+                # print(f"Position: x = {newx}, y = {self.segments[seg_num - 1].ycor()}")
+                print(f"Position x: = {newx} , Position y: = {newy}")
+                time.sleep(2)
+                sys.exit()
             self.segments[seg_num].goto(newx, newy)
         self.segments[0].forward(MOVE_DIST)
 
