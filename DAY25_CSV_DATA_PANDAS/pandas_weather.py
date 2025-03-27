79a45bfb54600b07e6feb8c138d5630d0f20768d
zen2see <zen2see@gmail.com>
2025-03-26 21:20:03 -0400

    UPDATED DAY24 DAY25

---
+++ DAY25_CSV_DATA_PANDAS/pandas_weather.py
@@ -0,0 +1,84 @@
+import csv
+import pandas
+
+# PANDAS DOCS - https://pandas.pydata.org/docs/
+# CREATES TABLE\DATAFRAME AND COLUMN IS A SERIES - COLUMNS LIKE A LIST
+
+""" 
+WITHOUT PANDAS
+with open("weather_data.csv") as data_file:
+    data = data_file.readlines()
+    print(data)
+
+def main():
+    with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != "tmep":
+            temperatures.append(int(row[1]))
+        print(temperatures)
+"""
+# ERRORS WITH PANDAS HAD TO "echo PATH=$HOME/.local/bin:$PATH"
+# THEN sudo pip3 install pandas
+
+def cvsreader():
+    data = pandas.read_csv("weather_data.csv")
+    
+    # PRINT AS DICTIONARY
+    data_dict = data.to_dict()
+    print("Data as a dictionary:", data_dict,'\n')
+    
+    # PRINT LENGTH OF TEMPS 
+    temp_list = data["Temp"].to_list()
+    print("Length of temps:", len(temp_list),'\n')
+    
+    # GET AVERAGE TEMP (MEAN)
+    temp_mean = data['Temp'].mean()
+    print("Average of temps2:", data['Temp'].mean(),'\n') # OR temp_mean 
+    
+    # GET MAX OF TEMP
+    print("Max of temp: ", data['Temp'].max())
+    print("Max of temp2:\n ", data[data.Temp == data.Temp.max()],"\n")
+    
+    # GET GET CONDITION (DATA IN COLUMNS)
+    print("Conditions:")
+    for condition in data['Condition']:
+        print(f" {condition}")
+        #, data['condition'].to_string(index=False),'\n')
+    print("\nCondisions2:")
+    
+    # PRINT( data.condition,'\n')
+    for condition in data['Condition']:
+        print(f" {condition}")
+    
+    # GET DATA IN A ROW
+    print("\nDAY OF WEEK: ")
+    print(data[data.Day == 'Monday'])
+    monday = data[data.Day =="Monday"]
+    # print(f"\nMonday's condition : {monday.Condition}")
+    monday_condition = data.loc[data['Day'] == 'Monday', 'Condition'].values[0]
+    monday_temp = data.loc[data['Day'] == 'Monday', 'Temp'].values[0] 
+    # OR
+    mondaytemp = monday.Temp[0]
+    print(f"\nMonday Temp in Farenheit = {monday_temp*9/5+32}\u00b0")
+
+    # CREATE A DATAFRAME FROM DICTIONARY
+    data_dict = { "students": ["Amy", "James", "Angela"], "scores": [76, 56, 65] }
+    dataframe = pandas.DataFrame(data_dict)
+    print(f"\nStudent Dataframe\n{dataframe}")
+    
+    # PRINT WITHOUT THE INDEX
+    print(f"\nWithout Index\n{dataframe.to_string(index=False)}")
+    
+    # CAPITALIZE THE HEADER 
+    dataframe.columns = [col.capitalize() for col in dataframe.columns]
+    
+    # CONVERT TO CSV AND CHANGE DELIMETER TO SPACES
+    dataframe.to_csv("dataframe_students_scores.csv", sep=',', index=False, header=True)
+   
+def main():
+    cvsreader()
+
+if __name__ == '__main__':
+    main()
---
+++ DAY25_CSV_DATA_PANDAS/squirrel.py
@@ -0,0 +1,81 @@
+import pandas
+
+# GET SQUIRREL DATA INTO A DATAFRAME
+squirrel_data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+# MAKE A DATAFRAME
+df_sd = pandas.DataFrame(squirrel_data)
+# CAPITALIZE THE HEADER 
+df_sd.columns = [col.capitalize() for col in df_sd.columns]
+
+def grey_squirrels():
+    grey_squirrels = squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"]
+    print(grey_squirrels)
+
+def grey_squirrels_count():
+    grey_squirrels_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"])
+    # print(f"{grey_squirrels_count}")
+    return grey_squirrels_count
+
+def cinnamon_squirrels_count():
+    cinnamon_squirrels_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Cinnamon"])
+    # print(f"{cinnamon_squirrels_count}")
+    return cinnamon_squirrels_count
+
+def black_squirrels_count():
+    black_squirrels_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Black"])
+    # print(f"{black_squirrels_count}")
+    return black_squirrels_count
+
+def file_to_csv():
+    df_sd.to_csv('Squirrel_count.csv', sep=',', header=True, float_format=str, index=False,)
+
+# MAKE A DICTIONARY OF FUR COUNTS
+data_dict = {
+    "Fur Color": ["Grey", "Cinnamon",  "Black"],
+    "Count": [grey_squirrels_count(), cinnamon_squirrels_count(), black_squirrels_count()]
+}
+def read_file():
+    # Read the CSV file and format the output with fixed-width columns
+    with open("dataframe_squirrel_colors.csv", "r") as file:
+        lines = file.readlines()
+    return lines
+
+# Determine the maximum width for each column
+def find_max_col_width(lines):
+    max_widths = [max(len(str(item)) for item in col) for col in zip(*[line.split() for line in lines])]
+    return max_widths
+
+# Format each line with fixed-width columns
+def fixed_width_cols(max_widths):
+    formatted_lines = []
+    for line in lines:
+        items = line.split()
+        formatted_line = " ".join(f"{item:<{max_widths[i]}}" for i, item in enumerate(items))
+        formatted_lines.append(formatted_line)
+    return formatted_lines
+
+# Write the formatted lines back to the CSV file
+def write_formatted():
+    with open("dataframe_students_scores.csv", "w") as file:
+        file.write("\n".join(formatted_lines))
+
+# DETERMINE MAX WIDTH FOR COLUMNS
+def determine_max_col_width():
+    max_widths = [max(len(str(item)) for item in col) for col in zip(*[line.split() for line in lines])]
+
+def fur_color_to_csv():
+    # MAKE A DATAFRAME
+    df_fc = pandas.DataFrame(data_dict)
+    # CAPITALIZE THE HEADER 
+    df_fc.columns = [col.capitalize() for col in df_fc.columns]
+    # CONVERT TO CSV AND CHANGE DELIMETER TO SPACES
+    df_fc.to_csv("dataframe_squirrel_colors.csv", sep=',', index=False, header=True)
+
+
+def main():
+    print(grey_squirrels_count(),)
+    # file_to_csv()
+    fur_color_to_csv()
+
+if __name__ == '__main__':
+    main()
